17790c64009fa5119efbf0928fcf484f60929dc6
IgorErin <erin.igore@yandex.ru>
2022-09-17 17:20:32 +0300

    table test

--- src/MatrixMul/Program.cs
+++ src/MatrixMul/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System.Data;
+using System.Diagnostics;
+using System.Text;
 using MatrixMul.Generators;
-using MatrixMul.MatrixExceptions;
 using MatrixMul.Readers;
 using MatrixMul.Writers;
 
@@ -9,18 +10,30 @@ namespace MatrixMul;
 public static class MatrixMain
 {
     private const string DefaultOutputPath = ""; //TODO()
+    private const int DefaultRunWithFixedSizeCount = 10;
+    private const int ConvertArrayCount = 3;
+
     public static void Main()
     {
-        Console.WriteLine("Welcome to high performance matrix multiplication...");
-        Console.WriteLine("You want to multiply your matrices, otherwise a test measurement will be performed? [yes/no]");
         
+        //
+        var firstArray = new double[] { 0.4, 0.4, 0.5 };
+        var scondArray = new double[] { 0.4, 0.4, 0.5 };
+        var thirdArray = new int[] { 0, 4, 0 };
+        
+        Console.WriteLine(ConvertArraysToTuple(firstArray, scondArray, thirdArray));
+        //
+        /*Console.WriteLine("Welcome to high performance matrix multiplication...");
+        Console.WriteLine(
+            "You want to multiply your matrices, otherwise a test measurement will be performed? [yes/no]");
+
         var testRunOrCustomMulAnswer = Console.ReadLine();
         var correctInput = IsCorrectAnswer(testRunOrCustomMulAnswer, "no", "yes");
-        
+
         while (!correctInput)
         {
             Console.Write("Incorrect answer, try again");
-            
+
             testRunOrCustomMulAnswer = Console.ReadLine();
             correctInput = IsCorrectAnswer(testRunOrCustomMulAnswer, "no", "yes");
         }
@@ -31,102 +44,103 @@ public static class MatrixMain
         }
         else
         {
-            TestMul();
-        }
+            TestMatrixMul();
+        }*/
     }
 
     private static bool IsCorrectAnswer(string? answer, params string[] correctAnswers)
         => Array.Exists(correctAnswers, s => s == answer);
-    
-    private static void TestMul()
+
+    private static void TestMatrixMul()
     {
-        var runCount = ReadInputCountWithMessage("Please enter the number of test runs");
-        
-        var parTimeArray = new double[runCount];
-        var seqTimeArray = new double[runCount];
+        var runCount = ReadInputCountWithMessage("Please enter the number of test runs with fixed matrix size");
 
         var parMulFun = MatrixOperations.Int2DArrayParallelMul;
         var seqMulFun = MatrixOperations.Int2DArraySequentialMul;
 
+        var parResultTime = new double[DefaultRunWithFixedSizeCount];
+        var seqResultTime = new double[DefaultRunWithFixedSizeCount];
+        var resultElementCounts = new int[DefaultRunWithFixedSizeCount];
 
-        for (var elementPower = 0; elementPower < 10; elementPower++)
+        for (var globalRunIndex = 0; globalRunIndex < DefaultRunWithFixedSizeCount; globalRunIndex++)
         {
+            var parTimeArray = new double[runCount];
+            var seqTimeArray = new double[runCount];
+
+            var elementCount = 200 + 2.IntPow(globalRunIndex);
+
             for (var runIndex = 0; runIndex < runCount; runIndex++)
             {
-                Console.WriteLine("gen arrays...");
-                
-                var elementCount = 200 * (2 ** elementPower)
+                Console.WriteLine("Gen arrays...");
 
-                var leftInt2DArray = ArrayGenerator.Generate2DIntArray(100 + 2 * elementPower, elementCount);
+                var leftInt2DArray = ArrayGenerator.Generate2DIntArray(elementCount, elementCount);
                 var rightInt2DArray = ArrayGenerator.Generate2DIntArray(elementCount, elementCount);
 
-                Console.WriteLine($"Execute parallel multiplication #{runIndex}");
+                Console.WriteLine($"Executing multiplication #{runIndex} with {elementCount} elements");
 
                 var currentParTimeResult = GetExecutionTime(parMulFun, leftInt2DArray, rightInt2DArray);
-
-                Console.WriteLine($"Execute sequential multiplication #{runIndex}");
-
                 var currentSeqTimeResult = GetExecutionTime(seqMulFun, leftInt2DArray, rightInt2DArray);
 
                 parTimeArray[runIndex] = currentParTimeResult;
                 seqTimeArray[runIndex] = currentSeqTimeResult;
             }
-            
-            var resultParAverageTime = Enumerable.Average(parTimeArray);
-            var resultSeqAverageTime = Enumerable.Average(seqTimeArray);
+
+            parResultTime[globalRunIndex] = Enumerable.Average(parTimeArray);
+            seqResultTime[globalRunIndex] = Enumerable.Average(seqTimeArray);
+            resultElementCounts[globalRunIndex] = elementCount;
         }
-        
-        
+
+        //Console.WriteLine(ResultTableWriter.WriteTableToFile((parResultTime, seqResultTime, resultElementCounts))); TODO()
     }
 
     private static void UserMatrixMul()
     {
         Console.Write("Please enter the path to the left matrix: ");
-        
+
         var leftMatrixPath = Console.ReadLine();
-        while (Path.GetFileName(leftMatrixPath) == null)
+        while (File.Exists(leftMatrixPath))
         {
             Console.Write("Incorrect path, try again");
-        
+
             leftMatrixPath = Console.ReadLine();
         }
-        
+
         Console.Write("Please enter the path to the right matrix: ");
-        
-        var rightMatrixPath = Console.ReadLine();
-        while (Path.GetFileName(rightMatrixPath) == null)
+
+        var rightMatrixPath = Console.ReadLine() ?? "";
+        while (File.Exists(rightMatrixPath))
         {
             Console.Write("Incorrect path, try again");
-        
+
             leftMatrixPath = Console.ReadLine();
         }
 
         var left2DArray = TextFileToInt2DArrayReader.GetMatrix(leftMatrixPath);
         var right2DArray = TextFileToInt2DArrayReader.GetMatrix(rightMatrixPath);
-        
+
         Console.WriteLine("Perform parallel multiplication or serial multiplication");
         Console.WriteLine("enter \"par\" for parallel and \"seq\" for sequential respectively");
-        
+
         var parallelOrSequentialMulAnswer = Console.ReadLine();
         var correctInput = IsCorrectAnswer(parallelOrSequentialMulAnswer, "par", "seq");
-        
+
         while (!correctInput)
         {
             Console.Write("Incorrect answer, try again");
-            
+
             parallelOrSequentialMulAnswer = Console.ReadLine();
             correctInput = IsCorrectAnswer(parallelOrSequentialMulAnswer, "par", "seq");
         }
 
         var mulFun = GetMulFun(parallelOrSequentialMulAnswer ?? "seq");
         var stopwatch = new Stopwatch();
-        
+
         stopwatch.Start();
         var result = mulFun(left2DArray, right2DArray);
         stopwatch.Stop();
 
         Int2DArrayToTextFileWriter.WriteToFile(DefaultOutputPath, result);
-        
+
         Console.WriteLine($"Multiplication done in {stopwatch.ElapsedMilliseconds} milliseconds");
         Console.WriteLine($"the result is written in {DefaultOutputPath}");
     }
@@ -144,9 +158,9 @@ public static class MatrixMain
     private static int ReadInputCountWithMessage(string message)
     {
         int localCount;
-        
+
         Console.Write(message);
-        
+
         while (!int.TryParse(Console.ReadLine(), out localCount))
         {
             Console.Write(message);
@@ -157,14 +171,32 @@ public static class MatrixMain
 
     private static long GetExecutionTime(Func<int[,], int[,], int[,]> mulFunc, int[,] left2DArray, int[,] right2DArray)
     {
-        var stopwatch = new Stopwatch();
-        
+        var stopwatch = new Stopwatch(); //TODO()
+
         stopwatch.Start();
         var result = mulFunc(left2DArray, right2DArray);
         stopwatch.Stop();
 
         return stopwatch.ElapsedMilliseconds;
     }
-    
-    
+
+    private static string ConvertArraysToTuple(double[] parTimes, double[] seqTimes, int[] itemCount) //TODO
+    {
+        var firstCond = parTimes.Length != seqTimes.Length;
+        var secondCond = parTimes.Length != itemCount.Length;
+        var thirdCond = seqTimes.Length != itemCount.Length;
+
+        if (firstCond || secondCond || thirdCond)
+        {
+            throw new ArgumentException("TODO()");
+        }
+
+        var length = parTimes.Length;
+
+        var stringBuilder = new StringBuilder();
+        
+        stringBuilder.AppendFormat("+{0}+", new String('-', 100));
+
+        return stringBuilder.ToString();
+    }
 }
