59ce9b0728d35973df5532315e4aa1a217ef220e
IgorErin <erin.igore@yandex.ru>
2022-11-09 13:09:19 +0300

    refactor: Main

--- src/MD5/Program.cs
+++ src/MD5/Program.cs
@@ -1,31 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using MD5;
-
-Console.WriteLine("Hello, World!");
 
 class MDMain
 {
+    private const int count = 10;
+
     public static void Main()
     {
         var watch = new Stopwatch();
 
-        watch.Start();
+        var timeArray = new long[count];
 
-        var _ = Hash.getSum(Directory.GetCurrentDirectory()).GetAwaiter().GetResult();
+        for (var i = 0; i < count; i++)
+        {
+            watch.Start();
 
-        watch.Stop();
+            var _ = Hash.getSum(Directory.GetCurrentDirectory()).GetAwaiter().GetResult();
 
-        Console.WriteLine($"result = {watch.ElapsedMilliseconds}");
+            watch.Stop();
 
-        watch.Reset();
-        watch.Start();
+            timeArray[i] = watch.ElapsedMilliseconds;
 
-        _ = ParallelHash.getSum(Directory.GetCurrentDirectory()).GetAwaiter().GetResult();
+            watch.Reset();
+        }
 
-        watch.Stop();
+        Console.WriteLine($"sequential result = {Enumerable.Average(timeArray)}");
 
-        Console.WriteLine($"parallel result = {watch.ElapsedMilliseconds}");
+        timeArray = new long[count];
+
+        for (var i = 0; i < count; i++)
+        {
+            watch.Start();
+
+            var _ = Hash.getSum(Directory.GetCurrentDirectory()).GetAwaiter().GetResult();
+
+            watch.Stop();
+
+            timeArray[i] = watch.ElapsedMilliseconds;
+
+            watch.Reset();
+        }
+
+        Console.WriteLine($"parallel result = {Enumerable.Average(timeArray)}");
     }
 }
