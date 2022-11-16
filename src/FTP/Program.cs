3fc55742c15cb499f0aeb267026318413d18446b
IgorErin <erin.igore@yandex.ru>
2022-11-16 17:58:59 +0300

    add: tests

--- src/FTP/Program.cs
+++ src/FTP/Program.cs
@@ -8,28 +8,23 @@ public static class FTPMain
     /// <summary>
     /// Main method.
     /// </summary>
+    /// <returns>Main task.</returns>
     public static async Task Main()
     {
         var server = new Server(8888);
 
         var tokenSource = new CancellationTokenSource();
 
-        var serverTask = Task.Run(async () => await server.Start(tokenSource.Token));
+        var _ = Task.Run(async () => await server.Start(tokenSource.Token));
 
         var client1 = new Client(8888);
 
-        // var result = await client1.List("D:\\Projects\\Homework-SecondYear\\src\\TestDir");
-        //
-        // Console.WriteLine($"result: size = {result.Item1}");
-        // foreach (var item in result.Item2)
-        // {
-        //     Console.Write(item);
-        // }
+        var result = await client1.List("./");
 
-        var newFileInfo = new FileInfo("D:\\Projects\\Homework-SecondYear\\src\\TestDir\\testSubDir\\result.txt");
-
-        var fileStream = newFileInfo.Open(FileMode.Open);
-
-        await client1.GetAsync("D:\\Projects\\Homework-SecondYear\\src\\TestDir\\testFile.txt", fileStream);
+        Console.WriteLine($"result size = {result.Item1}");
+        foreach (var item in result.Item2)
+        {
+            Console.Write(item);
+        }
     }
 }
--- test/FTPTest/FTPTest.cs
+++ test/FTPTest/FTPTest.cs
@@ -4,25 +4,83 @@ using FTP;
 
 public class Tests
 {
-    private Server server = new Server(8888);
-    private CancellationTokenSource sourceToken = new CancellationTokenSource();
+    private const int port = 8888;
+    private readonly Server server = new (port);
+    private readonly CancellationTokenSource sourceToken = new ();
 
+    /// <summary>
+    /// Set up method.
+    /// </summary>
     [SetUp]
     public void Setup()
     {
         Task.Run(async () => await this.server.Start(sourceToken.Token));
     }
 
+    /// <summary>
+    /// Test equality of responses of multiple queries <see cref="Client.List(string)"/>.
+    /// </summary>
     [Test]
-    public async Task Test1()
+    public async Task NumberListAnswersAreEqualTest()
     {
-        var client = new Client(8888);
+        const int clientCount = 10;
+        var clients = new Client[clientCount];
 
-        var result = await client.List("./");
-
-        foreach (var item in result.Item2)
+        for (var i = 0; i < clientCount; i++)
         {
-            Console.WriteLine(item);
+            clients[i] = new Client(port);
         }
+
+        var tasks = new Task<(int, List<(string, bool)>)>[clientCount];
+
+        for (var i = 0; i < clientCount; i++)
+        {
+            tasks[i] = clients[i].List("./");
+        }
+
+        var sizeArray = new int[clientCount];
+        var listArray = new List<(string, bool)>[clientCount];
+
+        for (var i = 0; i < clientCount; i++)
+        {
+           (sizeArray[i], listArray[i]) = ((await tasks[i].ConfigureAwait(false)).Item1, (await tasks[i].ConfigureAwait(false)).Item2);
+        }
+
+        foreach (var item in sizeArray)
+        {
+            Assert.That(item, Is.EqualTo(sizeArray[0]));
+        }
+
+        foreach (var item in listArray)
+        {
+            Assert.That(item, Is.Not.Null);
+            Assert.That(item, Is.EqualTo(listArray[0]));
+        }
+    }
+
+    /// <summary>
+    /// A test that checks the non-negativity of the size of an existing directory.
+    /// </summary>
+    [Test]
+    public async Task ExistingDirectorySizeTest()
+    {
+        var client = new Client(port);
+
+        var (size, _) = await client.List("./");
+
+        Assert.That(size, Is.GreaterThan(0));
+    }
+
+    /// <summary>
+    /// A test that checks the non-negativity of the size of an not existing directory.
+    /// </summary>
+    [Test]
+    public async Task NotExistingDirectorySizeTest()
+    {
+        var client = new Client(port);
+
+        var (size, _) = await client.List("@<notExistPath>");
+
+        Assert.That(size, Is.EqualTo(-1));
     }
 }
